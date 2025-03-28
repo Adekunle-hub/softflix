8f18a891c8cd743c62627e214a32bf0a5b0c7bc7
Ishola Mujeeb <isholamujeebadekunle@gmail.com>
2025-03-28 16:00:24 +0100

    sign in credentials

--- signin/signin.js
+++ signin/signin.js
@@ -1,4 +1,7 @@
-
+const eyeIcon = document.querySelector('.js-eye-icon');
+const userPassword = document.querySelector('.password');
+const userEmail = document.querySelector('.email')
+const singInButton = document.querySelector('.signin');
 
 function loadHomePage(){
   const homeNetflix  = document.querySelector('.netflix-header-js')
@@ -7,4 +10,30 @@ function loadHomePage(){
   })
 }
 
+function showPassword(){
+  eyeIcon.addEventListener('click',()=>{
+   if(userPassword.type === "password"){
+    userPassword.type = "text"
+    eyeIcon.classList.replace('fa-eye', "fa-eye-slash")
+   }else{
+    userPassword.type = "password"
+    eyeIcon.classList.replace('fa-eye-slash', "fa-eye")
+   
+   }
+  })
+}
+
+function loadSignIn(){
+  singInButton.addEventListener('click',()=>{
+    if(userEmail.value === '' || userPassword.value === ''){
+      alert('Please, fill both fields')
+    }else{
+    userEmail.value = '';
+    userPassword.value = '';
+    alert('Account created successfully')
+    }
+  })
+}
 loadHomePage()
+showPassword()
+loadSignIn()
